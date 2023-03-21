15fdd30bab3e1a9ace9ed3389492868fd09b716a
JoanOkoro <joanokoro4@gmail.com>
2023-03-21 15:22:49 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ *
+*/
+
+int main(void)
+
+{
+
+char str[] = "_putchar\n";
+int i = 0;
+while (str[i] != '\0')
+{
+putchar(str[i]);
+i++;
+}
+
+return (0);
+
+}
