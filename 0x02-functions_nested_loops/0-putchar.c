c774d0d5a600f3715f1a08e79c2849a05c5c8327
JoanOkoro <joanokoro4@gmail.com>
2023-03-21 18:13:20 -0700

    Update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * main - Entry point
@@ -6,19 +5,25 @@
  * Return: Always 0 (Sucess)
  *
 */
-
 int main(void)
 
 {
 
-char str[] = "_putchar\n";
-int i = 0;
-while (str[i] != '\0')
-{
-putchar(str[i]);
-i++;
-}
+int _putchar(char c);
+
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
 
 return (0);
 
 }
+
+
