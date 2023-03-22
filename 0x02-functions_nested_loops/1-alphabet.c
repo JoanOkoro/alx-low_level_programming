b1cc4e9ba46ce0cef5c878e229cdeab58a98e0a9
JoanOkoro <joanokoro4@gmail.com>
2023-03-22 09:15:31 -0700

    Update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,10 @@
 #include "main.h"
 /**
- * print_alphabet- Entry point
- *
- * Description - Print the lowercase alphabet
+ * print_alphabet- print all the alphabet in lowercase
  *
  * Return: Always 0 (Success)
  *
 */
-
-
 void print_alphabet(void)
 
 {
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
-_putchar(char)
+int _putchar(char)
 #endif
