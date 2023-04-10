429e8ad5424833b3387c8cc04a7c6dada404b299
JoanOkoro <joanokoro4@gmail.com>
2023-04-10 05:56:31 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+
+/**
+ * main - To print the number of argument passed
+ * to a program
+ * @arg_count: To count the number of argument
+ * @arg_vector: To print the name of argument
+ * Return: 0.
+ */
+
+int main(int arg_count, char *arg_vector[] __attribute__ ((unused)))
+{
+	printf("%d\n", arg_count);
+	return (0);
+
+}
