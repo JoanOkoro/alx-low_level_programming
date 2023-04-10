a42b6746ddc5a3f36ef5ad52d4898d1fbfbe79ef
JoanOkoro <joanokoro4@gmail.com>
2023-04-10 05:05:23 -0700

    Update Whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,17 @@
+#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - To print a name, followed by a new line
- *
- * @arg_count: count of arg
- * @arg_vector: name of arg
- * Return: EXIT_SUCCESS
+ * main  - To print a name, followed by a new line
+ * @arg_count: to count the number of arg
+ * @arg_vector: to print the name of arg
+ * Return: 0
  */
 
-int main(int arg_count, char *arg_vector[])
+int main(int arg_count __attribute__ ((unused)), char *arg_vector[])
 {
-	printf("arg_count = %d\n", arg_count);
+	printf("%s\n", arg_vector[0]);
 	printf("%s\n", arg_vector[0]);
 
-	return (EXIT_SUCCESS);
+	return (0);
 }
